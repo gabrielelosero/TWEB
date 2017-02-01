c11b51e055b22cce84a244ef07c7eb1efa66ac7d
root <root@debian-test>
2017-02-01 16:53:38 +0100

    aggiornato test

--- php/content/test.php
+++ php/content/test.php
@@ -1,18 +1,25 @@
 <h2 class="titoloDue">TEST</h2>
 
 <ul>
-  <li>Sicurezza query - verificare ogni query su db e validare dati in input client side</li>
-  <li>Sicurezza password - aggiungere md5 a passwd su db</li>
-  <li>Footer fatto bene</li>
-  <li>Sfondo</li>
-  <li>Classifica in base ai punti</li>
-  <li>Aggiornamento voti giocatori</li>
-  <li>Svolgimento partita</li>
-  <li>Vendere giocatori</li>
-  <li>Scegliere la formazione</li>
-  <li>Scegliere giocatori titolari</li>
-  <li>Immagine invece di compra/vendi</li>
-  <li></li>
-  <li></li>
+  <li>
+    <ul>Pagine da rifare:
+      <li><p>compraGiocatore.php: ci devono essere tre pagine (cerca per nome/posizione/squadra)</p></li>
+      <li><p>giocatori.php: show only first 20 entry and next page options</p></li>
+      <li><p>homepage: di base lasciare così ma se possibile migliorare, magari qualcosa di interattivo</p></li>
+      <li><p>schedaGiocatore: più o meno stessi contenuti, magari da mettere in maniera più carina</p></li>
+      <li><p>schedaTeam: inserire diverse sezioni: titolari/panchinari - voti ultima giornata - info su squadra</p></li>
+      <li><p>signup/signin  ok</p></li>
+      <li><p>squadre.php: non mi ricordo neanche più cosa sia, verificare e se possibile migliorare</p></li>
+      <li><p>team.php: idem con patate</p></li>
+      <li><p>welcome.php: pure(è)</p></li>
+    </ul>
+  </li>
+  <li><p>Fare relazione</p></li>
+  <li><p>Sicurezza query - verificare ogni query su db e validare dati in input client side</p></li>
+  <li><p>Footer fatto bene</p></li>
+  <li><p>Classifica in base ai punti</p></li>
+  <li><p>Svolgimento partita</p></li>
+  <li><p>Scegliere giocatori titolari</p></li>
+  <li><p>Immagine invece di compra/vendi</p></li>
 </ul>
 
